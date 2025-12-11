930239685b84d1a24e374356916c29f98fcaf81f
Ismail Akram <123332697+Ismailakram300@users.noreply.github.com>
2025-12-11 12:36:58 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:sign_spark/view/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'firebase_options.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(

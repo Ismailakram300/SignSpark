48064fd0449a8c3f034ce070810be9a2e1c09893
Ismail Akram <123332697+Ismailakram300@users.noreply.github.com>
2026-03-30 10:40:52 +0500

    Add createState method to Quiz class

--- lib/Quiz/quiz.dart
+++ lib/Quiz/quiz.dart
@@ -4,6 +4,7 @@ import '../aplhabet_signs/alphabet_sign_class.dart';
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
+  
   @override
   State<Quiz> createState() => _QuizState();
   
